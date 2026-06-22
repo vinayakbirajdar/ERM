da866b728327ed959acbb1bf4b0a825b5673cc4a
vinayak birajdar <108874547+vinayakbirajdar@users.noreply.github.com>
2026-06-22 15:30:33 +0530

    Nav bar implementation

---
+++ src/app/shared/components/navbar/navbar.ts
@@ -0,0 +1,12 @@
+// src/app/shared/components/navbar/navbar.ts
+
+import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+@Component({
+  selector: "app-navbar",
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: "./navbar.html",
+  styleUrl: "./navbar.css",
+})
+export class Navbar {}
